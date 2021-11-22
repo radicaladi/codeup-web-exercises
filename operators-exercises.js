54766e7321c21245354d026daf8da6d0a320057d
Adrian Brown <adrian.r.brown90@gmail.com>
2021-11-22 11:40:55 -0700

    operators

---
+++ operators-exercises.js
@@ -0,0 +1,19 @@
+console.log('1' + 2); //12
+console.log(typeof '1' + 2); //string and a number
+console.log(6 % 4); //remainder 2
+console.log('3 + 4 is ' + (3 + 4)); //I am unsure
+// how could you make this produce the correct output?
+console.log(0 < 0); //false
+console.log('false' == false); //true
+console.log(true == 'true'); //true
+console.log(5 >= -5); //false
+console.log(!false || false); //false
+console.log(true || "42"); //true
+console.log(!true && !false); //false
+console.log(6 % 5); //remainder 1
+console.log(5 < 4 && 1 === 1); //false
+console.log(typeof 'codeup' === 'string'); //true
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup'); //false
+console.log(4 >= 0 && 1 !== '1'); //true
+console.log(6 % 3 === 0); //true
+console.log(5 % 2 !== 0); //true

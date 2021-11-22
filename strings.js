65f8fc51a0658fcd762b72f970f3a78c8b2cb9ed
Adrian Brown <adrian.r.brown90@gmail.com>
2021-11-22 11:21:44 -0700

    few exercises

---
+++ strings.js
@@ -0,0 +1,9 @@
+var myName = "Adrian.";
+var aSentence = "Hi my name is, " + myName;
+var aboutSelf = "\n\tI have a strong desire to learn how to program code.\n";
+var enjoyThis = "\t\tI also enjoy drinking tea.";
+
+aSentence += aboutSelf += enjoyThis;
+
+console.log(aSentence);
+

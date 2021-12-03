97f371418e8088610a486d141ce2f3743ed65190
Adrian Brown <adrian.r.brown90@gmail.com>
2021-12-03 09:27:12 -0700

    refactored and fixed solution to break-continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,45 +1,30 @@
 "use strict";
 
-// let chosenNumber = prompt("Choose an odd number.", " ");
-// for (let i = 1; i < 50; i++) {
-//     if (i % 2 === 0) {
-//         // Skip even numbers.
-//         continue;
-//     }
-//
-//     if (chosenNumber === i) {
-//         console.log("Avoid! Skipping number: " + i);
-//     } else {
-//         console.log("Here is an odd number: " + i);
-//     }
-//
-//     if (i >= 49) {
-//         break;
-//     }
-// }
+let submittedNumber = 0;
 
-let submittedNumber = Number(prompt("Enter an odd number"));
-
-while (submittedNumber % 2 === 0) /** while true, do the following: */ {
+do {
+    submittedNumber = Number(prompt("Enter an odd number"));
     //This returns a message and the prompt if value entered is not odd, as a loop.
-    alert("Odd numbers only!");
-    submittedNumber = prompt("Enter an odd number");
-}
-alert("The number to skip is, " + submittedNumber);
+    if (submittedNumber < 1 || submittedNumber > 50) {
+        alert("Operation failed! Need numbers between 1 and 50");
+    } else if ((submittedNumber % 2 === 0) /** while true, do the following: */) {
+        alert("Odd numbers only!");
+    } else {
+        alert("Got it!");
+        break;
+    }
+} while (true);
 
-for (var i = 1; i < 100; i++) {
+console.log("The number to skip is, " + submittedNumber);
+for (var i = 1; i < 100; i += 2) {
     if (i % 2 === 0) {
         // Skip even numbers.
         continue;
     }
 
-    if (submittedNumber === i) {
+    if (i === submittedNumber) {
         console.log("Avoiding! " + i);
     } else {
         console.log("Here is an odd number: " + i);
-    }
-
-    if (i >= 49) {
-        break;
     }
 }

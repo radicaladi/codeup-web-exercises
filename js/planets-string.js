465e02b43c369b5947161e3eadbdbc2cc537b9eb
Adrian Brown <adrian.r.brown90@gmail.com>
2021-12-03 16:02:37 -0700

    completed planet-strings

--- js/planets-string.js
+++ js/planets-string.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
+    var planetsArray = planetsString.split("|");
 
     /**
      * TODO:
@@ -11,7 +11,8 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    //code for conversion is in the above planetsArray variable.
+    console.log(planetsArray);
 
     /**
      * TODO:
@@ -23,5 +24,12 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    // planetsString = planetsArray.join("<br>");
+    // console.log(planetsString);
+
+    //bonus is as follows:
+   planetsString = "<ul><li>" + planetsArray.join("</li><li>") + "</li></ul>";
+   console.log(planetsString);
 
 })();

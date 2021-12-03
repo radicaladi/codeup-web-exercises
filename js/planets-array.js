0a5af0e25d0b7014c66d84fc5eb0d17d72aad4a1
Adrian Brown <adrian.r.brown90@gmail.com>
2021-12-03 15:17:08 -0700

    completed planets-array exercises

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,24 +18,39 @@
      * the step that it describes
      */
 
+    planets.unshift("Sun");
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
 
+    planets.push("Pluto");
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
+    planets.shift("Sun");
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
 
+    planets.pop("Pluto");
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf("Earth"));
 
+    planets.reverse();
     console.log("Reversing the order of the planets array.");
     console.log(planets);
 
+    planets.sort();
     console.log("Sorting the planets array.");
     console.log(planets);
 })();
 
+/** Class exercises
+ * Friday, 3 December 2021
+ * */
+
+//*** SPREAD OPERATOR NOTES ***
+
+// "..." IS THE SPREAD OPERATOR
+// You can add two arrays or remove the brackets from around the array.

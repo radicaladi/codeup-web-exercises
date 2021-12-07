7fb601583a70f02574ba894d77ee39d43e3f391e
Adrian Brown <adrian.r.brown90@gmail.com>
2021-12-07 13:11:16 -0700

    cicle exercise completed

--- js/circle.js
+++ js/circle.js
@@ -9,16 +9,17 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return Math.PI * Math.pow(this.radius, 2); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding === true) {
+                console.log(Math.round(this.getArea()));
+            } else
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
 
@@ -31,6 +32,8 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+    let newRadius = 5
+    circle.radius = newRadius;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
@@ -42,25 +45,25 @@
  * Tuesday, 7 December 2021
  * */
 
-console.log(Math.random());
-
-function getArbitraryNumber (min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function getRandomInt (min, max) {
-    const randomNumber = Math.random() * (max - min) + min
-    console.log("before floor || ceil, ", randomNumber);
-    console.log("after round down, ", Math.floor(randomNumber));
-    console.log("after round up, ", Math.ceil(randomNumber));
-    console.log("after rounding nearest, ", Math.round(randomNumber));
-    return Math.floor(randomNumber);
-}
-
-
-console.log(Math.pow(8, 2)); //this is math.pow to get a value to a power.
-console.log(8 ** 2); //this is shorthand method of coding power of a number.
-
-console.log("the return of pi, ", Math.PI); //will always return pi.
-
-console.log(Math.E); //the base of natural logarithms; like exponential.
+// console.log(Math.random());
+//
+// function getArbitraryNumber (min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+//
+// function getRandomInt (min, max) {
+//     const randomNumber = Math.random() * (max - min) + min
+//     console.log("before floor || ceil, ", randomNumber);
+//     console.log("after round down, ", Math.floor(randomNumber));
+//     console.log("after round up, ", Math.ceil(randomNumber));
+//     console.log("after rounding nearest, ", Math.round(randomNumber));
+//     return Math.floor(randomNumber);
+// }
+//
+//
+// console.log(Math.pow(8, 2)); //this is math.pow to get a value to a power.
+// console.log(8 ** 2); //this is shorthand method of coding power of a number.
+//
+// console.log("the return of pi, ", Math.PI); //will always return pi.
+//
+// console.log(Math.E); //the base of natural logarithms; like exponential.

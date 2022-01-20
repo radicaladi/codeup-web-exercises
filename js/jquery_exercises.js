d49f4605650bb96d3aded5f31f4f687cac5bbe48
Adrian Brown <adrian.r.brown90@gmail.com>
2022-01-20 10:04:47 -0700

    jquery selectors exercises complete

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,42 +1,60 @@
 "use strict";
 
-document.getElementById("heading") //DOM method
+// document.getElementById("heading") //DOM method
+//
+//
+// //JQuery methods as follows... ID SELECTORS
+// let myEL = $('#p-one'); //this is equivalent of getting document by ID.
+// console.log(myEL);
+// alert(myEL.text());
+//
+// let header = $('#heading'); // same as above.
+// console.log(header);
+//
+// let valHeading = header.text(); //returns content of whatever you assign it to.
+// console.log(valHeading);
+// console.log(header.html()); //returns the content of whatever you assign it to but from the html.
 
-
-//JQuery methods as follows...
-let myEL = $('#p-one'); //this is equivalent of getting document by ID.
-console.log(myEL);
-
-let header = $('#heading'); // same as above.
-console.log(header);
-
-let valHeading = header.text(); //returns content of whatever you assign it to.
-console.log(valHeading);
-
-console.log(header.html()); //returns the content of whatever you assign it to but from the html.
-
-//adding class to 'border' to p tag...
+// //adding class to 'border' to p tag...
 // $('#p-one').addClass('border');
 
-// ADDING A BORDER TO HEADING IF USER IS LOGGED IN
+// //ADDING A BORDER TO HEADING IF USER IS LOGGED IN
 
-//user object as follows,
-let user1 = {
-    name: "Adi",
-    isLoggedIn: true,
-    age: 31,
-}
+// //user object as follows,
+// let user1 = {
+//     name: "Adi",
+//     isLoggedIn: true,
+//     age: 31,
+// }
+//
+// //check the status of the user...
+// let loginStatus = user1.isLoggedIn;
+//
+// //if user is logged in the header will get a red border...
+// if(loginStatus === true){
+//     $('#heading').addClass('border');
+// }
 
-//check the status of the user...
-let loginStatus = user1.isLoggedIn;
+// //CLASS SELECTORS
+// //codeup class, multiple elements, border...
+// $(".codeup").css("border", "1px solid red");
 
-//if user is logged in the header will get a red border...
-if(loginStatus === true){
-    $('#heading').addClass('border');
-}
+// //div blue background...
+// $("#codeup").css("background-color", "dodgerblue");
 
-//changing two css properties on one element...
-$("#p-one").css({"color": "red", "font-size": "12px"})
+// //changing two css properties on one element...
+// $("#p-one").css({"color": "red", "font-size": "12px"});
 
-//multiple elements with css
-$("p, ul, li").css("border", "4px solid blue");
+
+// //ELEMENT SELECTORS
+//setting font-size li...
+$('li').css("font-size", "20px");
+
+// //highlight h1, p, li...
+// $('h1').css("background-color", "rebeccapurple");
+// $('p').css("background-color", "rebeccapurple");
+// $('li').css("background-color", "rebeccapurple");
+
+//combine highlight h1, p, li...
+$('h1, p, li').css("background-color", "rebeccapurple");
+alert($('h1').text());

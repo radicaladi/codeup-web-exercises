0645a586e255591e59a3fe4fe6fcb37b0ec5eaec
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-01 11:31:00 -0700

    event listeners for buttons and mapbox populate

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,21 +8,44 @@
 // })
 
 // * using curriculum...
-$.get("https://api.openweathermap.org/data/2.5/weather", {
-    APPID: weatherKey,
-    q: "Tucson, US",
-    units: "imperial"
-}).done(function (data) {
-    console.log(data);
+// $.get("https://api.openweathermap.org/data/2.5/weather", {
+//     APPID: weatherKey,
+//     q: "Tucson, US",
+//     units: "imperial"
+// }).done(function (data) {
+//     console.log(data);
+//
+//     $(".temp").append(`<p>${data.main.temp}</p>`);
+//     $(".description").append(`<p>${data.weather}</p>`);
+//     $(".humidity").append(`<p>${data.main.humidity}</p>`);
+//     $(".wind").append(`<p>${data.wind.speed}</p>`);
+// });
 
-    $(".temp").append(`<p>${data.main.temp}</p>`);
-    $(".description").append(`<p>${data.weather}</p>`);
-    $(".humidity").append(`<p>${data.main.humidity}</p>`);
-    $(".wind").append(`<p>${data.wind.speed}</p>`);
+// * 5 day forecast...
+// $.get("http://api.openweathermap.org/data/2.5/forecast", {
+//     APPID: weatherKey,
+//     q:   "Tucson, US",
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log('5 day forecast', data.list[0]);
+//     for (let i = 0; i < data.list.length; i++){
+//         console.log(data.list[i]);
+//     }
+//
+// });
+
+// test location weather: -110.99, 32.32
+
+// * mapbox map...
+mapboxgl.accessToken = mapBoxKey;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 3,
+    center: [-110.9747, 32.2226]
 });
 
-
-// function temperatureConverter(valNum) {
-//     valNum = parseFloat(valNum);
-//     document.getElementById("outputFahrenheit").innerHTML=((valNum-273.15)*1.8)+32;
-// }
+function init () {
+    document.getElementById('btnGet').addEventListener('click', getWeather);
+    document.getElementById('btnCurrent').addEventListener('click', getLocation);
+}

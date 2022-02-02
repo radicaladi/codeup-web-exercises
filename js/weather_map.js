6a8f19697f72f94836639b8c8fa5657f7564082e
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-01 18:20:01 -0700

    removed forms and listeners.. refactored to append card with data to body.. added geocoder and marker

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,68 +1,78 @@
 // test location weather: -110.99, 32.32
 
 // *** MAPBOX SECTION ***
-mapboxgl.accessToken = mapBoxKey;
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 3,
-    center: [-110.9747, 32.2226]
-});
 
+// home
+let homeLat = 32.32;
+let homeLng = -110.99;
+// need to figure out how to clear this data when a new location is clicked.
+
+// when page loads
+$(document).ready(function () {
+    //sets first location
+    getWeather(homeLat, homeLng);
+    // centers on my location
+    mapboxgl.accessToken = mapBoxKey;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: [homeLng, homeLat]
+    });
+    // Set marker options.
+    const marker = new mapboxgl.Marker({
+        draggable: true
+    }).setLngLat([homeLng, homeLat])
+        .addTo(map);
+    // Add geocoder to the map.
+    map.addControl(
+        new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+        })
+    );
+    //trying to figure out how to make the marker draggable and update coordinates to open-api
+    // this.marker = new mapboxgl.Marker();
+    // this.map.on('click', this.marker.bind(this));
+})
 
 // *** OPEN WEATHER SECTION ***
 
-function init () {
-    document.getElementById('btnGet').addEventListener('click', getWeather);
-    document.getElementById('btnCurrent').addEventListener('click', getLocation);
-}
-
-// <div className="col">
-//     <div className="card" style="width: 30vw">
-//         <h5 id="city" className="card-title p-2">City</h5>
-//         <h6 id="date" className="card-title p-3">Date</h6>
-//         <img
-//             src="http://openweathermap.org/img/wn/10d@4x.png"
-//             className="card-img-top"
-//             alt="Weather description"
-//         />
-//         <div className="card-body">
-//             <h3 className="card-title">Weather Label</h3>
-//             <p className="card-text">High Temp Low Temp</p>
-//             <p className="card-text">HighFeels like</p>
-//             <p className="card-text">Pressure</p>
-//             <p className="card-text">Precipitation</p>
-//             <p className="card-text">Wind speed and direction</p>
-//         </div>
-//     </div>
-// </div>
-function getWeather () {
-    let lat = document.getElementById('latitude').value;
-    let lon = document.getElementById('longitude').value;
-    let key = weatherKey;
-    let lang = 'en';
-    let units = 'imperial';
-    $.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`).done(function (data) {
-        var fivedays = [];
-        for (let i = 0; i < data.list.length; i += 8) {
-            fivedays.push(data.list[i]);
+function getWeather (lat, lng) {
+    $.get("https://api.openweathermap.org/data/2.5/forecast", {
+        APPID: weatherKey,
+        lat: lat,
+        lon: lng,
+        units: "imperial"
+    }).done(function (data) {
+        console.log(data);
+        let city = data.city.name;
+        $(".city").append(`${city}`)
+        for (i = 0; i < data.list.length; i += 8) {
+            console.log(data.list[i]);
+            let date = data.list[i].dt_txt;
+            let tempHigh = data.list[i].main.temp_max;
+            let tempLow = data.list[i].main.temp_min;
+            let weather = data.list[i].weather[0].description;
+            let humidity = data.list[i].main.humidity;
+            let wind = data.list[i].wind.speed;
+            let pressure = data.list[i].main.pressure;
+            let icon = data.list[i].weather[0].icon;
+            $(".card-group").append(`
+            <div class="card">
+                <div class="card-header text-center bg-secondary">${date}</div>
+                <div class="card-body text-center">
+                    <h6>${tempHigh}°F / ${tempLow}°F</h6>
+                    <img src="https://openweathermap.org/img/w/${icon}.png" alt="weather icon">
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item text-center">${weather}</li>
+                    <li class="list-group-item">Humidity: ${humidity}%</li>
+                    <li class="list-group-item">Wind: ${wind} mph</li>
+                    <li class="list-group-item">Pressure: ${pressure} hPa</li>
+                </ul>
+            </div>
+        `)
         }
-    });
+    })
 }
-
-function getLocation () {
-    let options = {
-        enableHighAccuracy: true,
-        timeout: 1000 * 10, //10s
-        maximumAge: 1000 * 60 * 5, //5min
-    };
-    navigator.geolocation.getCurrentPosition(success, fail, options);
-}
-function success () {
-    console.log("cool")
-}
-function fail () {
-    console.log("went wrong")
-}
-
-init(); //makes form field and buttons work

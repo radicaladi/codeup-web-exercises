343da82cc2619fe33db453c313bd13d9e1e3fee3
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-08 15:03:11 -0700

    updated promise exercise

--- js/promises.js
+++ js/promises.js
@@ -6,15 +6,40 @@
 // create a function that takes a github username ✅
 // that returns the date of the last commit
 
-
-fetch('https://api.github.com/users/radicaladi', {headers: {'Authorization': 'token '}})
+function randomUser (input) {
+    console.log(input);
+fetch(`https://api.github.com/users/${input}`, {headers: {'Authorization': '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'}})
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        console.log(data.updated_at);
+        console.log("last commit time as follows, " + data.updated_at);
+        return data.updated_at;
     })
     .catch(error => console.log(error));
-
-function wait (number) {
-
 }
+randomUser("radicaladi");
+
+//
+// function wait() {
+//     return fetch('https://api.github.com/users/radicaladi', {headers: {'Authorization': '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'}})
+//
+// }
+//
+// function wait() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (Math.random() > 0.1) {
+//                 resolve('Here is your data: ...');
+//             } else {
+//                 reject('Network Connection Error!');
+//             }
+//         }, 1500);
+//     });
+// }
+//
+// const wade = wait();
+// console.log(wade); // pending promise
+// wade.then(message => console.log('Promise resolved!', message));
+// // if resolved, will log "Promise resolved!" and "Here is your data: ..."
+// wade.catch(message => console.log('Promise rejected!', message));
+// // if rejected, will log "Promise rejected!" and "Network Connection Error!"

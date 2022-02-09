d2d6f14ee6513678196e87ff7995905a4e538720
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-09 08:35:33 -0700

    wait function added

--- js/promises.js
+++ js/promises.js
@@ -4,47 +4,42 @@
 //     .then(username => console.log(username))
 
 // create a function that takes a github username ✅
-// that returns the date of the last commit
+// that returns the date of the last commit ✅
 
-function randomUser(input) {
-    console.log(input);
-    fetch(`https://api.github.com/users/${input}`, {headers: {'Authorization': '_j8LaJBe2hrqQJAzVA3btdyq4eXTQdB29jSJt'}})
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            console.log("last commit time as follows, " + data.updated_at);
-            return data.updated_at;
-        })
-        .catch(error => console.log(error));
+// function randomUser(input) {
+//     console.log(input);
+//     fetch(`https://api.github.com/users/${input}`, {headers: {'Authorization': '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'}})
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             console.log("last commit time as follows, " + data.updated_at);
+//             return data.updated_at;
+//         })
+//         .catch(error => console.log(error));
+// }
+//
+// randomUser("radicaladi");
+
+//
+// function wait() {
+//     return fetch('https://api.github.com/users/radicaladi', {headers: {'Authorization': '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'}})
+//
+// }
+//
+function wait(input) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve('Here is your data: ...');
+            } else {
+                reject('Network Connection Error!');
+            }
+        }, input);
+    });
 }
 
-randomUser("radicaladi");
-
-//
-// function wait() {
-//     return fetch('https://api.github.com/users/radicaladi', {headers: {'Authorization': '_j8LaJBe2hrqQJAzVA3btdyq4eXTQdB29jSJt'}})
-//
-// }
-//
-// function wait() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if (Math.random() > 0.1) {
-//                 resolve('Here is your data: ...');
-//             } else {
-//                 reject('Network Connection Error!');
-//             }
-//         }, 1500);
-//     });
-// }
-//
-// const wade = wait();
-// console.log(wade); // pending promise
-// wade.then(message => console.log('Promise resolved!', message));
-// // if resolved, will log "Promise resolved!" and "Here is your data: ..."
-// wade.catch(message => console.log('Promise rejected!', message));
-// // if rejected, will log "Promise rejected!" and "Network Connection Error!"
-
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(4000).then(() => console.log('You\'ll see this after 4 seconds'));
 
 
 //********************** code for random github user generator
